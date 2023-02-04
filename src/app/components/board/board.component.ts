a3d847ba0ad3047d9f7f65463aa6bb5e9f845555
jacob <jshomali@nexient.com>
2023-02-03 18:07:20 -0600

    centered the board

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+type Color = 'white' | 'black';
+interface Board {
+  [yPosition: number]: {
+    [xPosition: number]: Color;
+  };
+}
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent {
-
+  constructor() {}
 }
