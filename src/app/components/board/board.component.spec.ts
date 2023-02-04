411abd0c17ab725ad305c7ff85a780abb47c6c60
jacob <jshomali@nexient.com>
2023-02-04 12:27:55 -0600

    the peice is created and falls normally but the test does not pass

--- src/app/components/board/board.component.spec.ts
+++ src/app/components/board/board.component.spec.ts
@@ -1,3 +1,4 @@
+import { fakeAsync, tick, flushMicrotasks, flush } from '@angular/core/testing';
 import { render, screen } from '@testing-library/angular';
 
 import { BoardComponent } from './board.component';
@@ -16,4 +17,15 @@ describe('BoardComponent', () => {
     const squares = screen.getAllByTestId('square');
     expect(squares.length).toBe(200);
   });
+
+  // it('after being rendered for a second an orange square will appear at the top of the grid', fakeAsync(async () => {
+  //   let playerPieces = screen.queryAllByTestId('player-peice');
+  //   expect(playerPieces.length).toBe(0);
+  //   tick(1000);
+  //   flush();
+  //   flushMicrotasks();
+  //   playerPieces = screen.getAllByTestId('player-peice');
+  //   expect(playerPieces.length).toBe(4);
+  //   expect(playerPieces[0].style.color).toBe('orange');
+  // }));
 });
--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,18 +1,88 @@
 import { Component } from '@angular/core';
+import {
+  Observable,
+  interval,
+  startWith,
+  map,
+  BehaviorSubject,
+  pairwise,
+  pipe,
+  take,
+  tap,
+  distinctUntilChanged,
+  distinctUntilKeyChanged,
+} from 'rxjs';
 
-type Color = 'white' | 'black';
-type Row = Color[];
-type Board = Row[];
+type Color = 'white' | 'black' | 'orange';
 
+export interface Square {
+  color: Color;
+  isPlayer: boolean;
+}
+
+export type Row = Square[];
+export type Board = Row[];
+
+interface Coordinate {
+  x: number;
+  y: number;
+}
+const log = <T>() =>
+  pipe<Observable<T>, Observable<T>>(
+    tap((x) => console.log(JSON.parse(JSON.stringify(x))))
+  );
+
+const clone = <T>() =>
+  pipe<Observable<T>, Observable<T>>(map((x) => JSON.parse(JSON.stringify(x))));
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent {
-  board: Board = this.getInitialBoard();
+  board = new BehaviorSubject<Board>(this.getInitialBoard());
 
-  constructor() {}
+  private gravity = interval(1000);
+
+  playerPeice = new BehaviorSubject<Coordinate[]>([]);
+  // I need to check every second whether a block exists and create it if it does
+  // then I need to increase its y coordinate by one every second
+  // update the board to reflect its position
+
+  constructor() {
+    this.gravity.subscribe(() => {
+      const prevValue = this.playerPeice.value;
+      const newValue =
+        prevValue.length === 0
+          ? [
+              { x: 4, y: 0 },
+              { x: 5, y: 0 },
+              { x: 4, y: 1 },
+              { x: 5, y: 1 },
+            ]
+          : prevValue.map((c) => {
+              c.y += 1;
+              return { ...c };
+            });
+      this.playerPeice.next(newValue);
+    });
+
+    this.playerPeice
+      .asObservable()
+      .pipe(clone(), pairwise())
+      .subscribe(([prev, current]) => {
+        const prevValue = this.board.value;
+        prev.forEach((c) => {
+          prevValue[c.y][c.x].color = 'white';
+          prevValue[c.y][c.x].isPlayer = false;
+        });
+        current.forEach((c) => {
+          prevValue[c.y][c.x].color = 'orange';
+          prevValue[c.y][c.x].isPlayer = true;
+        });
+        this.board.next([...prevValue]);
+      });
+  }
 
   private getInitialBoard(): Board {
     const board: Board = [];
@@ -21,7 +91,7 @@ export class BoardComponent {
       const row: Row = [];
       board.push(row);
       for (let j = 0; j < 10; j++) {
-        row.push('white');
+        row.push({ color: 'white', isPlayer: false });
       }
     }
     return board;
