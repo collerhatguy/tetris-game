3420150f4727fa04858f72439b948d5856e41afd
jacob <jshomali@nexient.com>
2023-02-03 18:13:43 -0600

    added test for rows

--- src/app/components/board/board.component.spec.ts
+++ src/app/components/board/board.component.spec.ts
@@ -1,11 +1,17 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
 
 import { BoardComponent } from './board.component';
+import { RowComponent } from './row/row.component';
 
 describe('BoardComponent', () => {
   beforeEach(async () => {
-    await render(BoardComponent);
+    await render(BoardComponent, {
+      declarations: [RowComponent],
+    });
   });
 
-  it('should create', () => {});
+  it('should have 20 rows', () => {
+    const rows = screen.getAllByTestId('row');
+    expect(rows.length).toBe(20);
+  });
 });
