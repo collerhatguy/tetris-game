56907bb507dba14adf4e49b646285e3be10cd8d0
jacob <jshomali@nexient.com>
2023-02-07 17:47:10 -0600

    wrote test

--- src/app/components/board/board.component.spec.ts
+++ src/app/components/board/board.component.spec.ts
@@ -1,6 +1,7 @@
 import { fakeAsync, tick } from '@angular/core/testing';
-import { render, screen } from '@testing-library/angular';
+import { fireEvent, render, screen } from '@testing-library/angular';
 import { BoardService } from './board-service/board.service';
+import { Coordinate } from './board-service/models';
 import { BoardComponent } from './board.component';
 import { PlayerPieceService } from './player-piece/player-piece.service';
 
@@ -29,12 +30,33 @@ describe('BoardComponent', () => {
     expect(squares.length).toBe(200);
   });
 
+  const getPlayerPieces = () => screen.findAllByTestId('player-peice');
+
+  const getPlayerCoordinates = async () => {
+    const playerPieces = await screen.findAllByTestId('player-peice');
+    return playerPieces.map((piece) => {
+      const string = piece.getAttribute('coordinate') as string;
+      const [x, y] = string.split('-');
+      return {
+        x: parseInt(x),
+        y: parseInt(y),
+      };
+    });
+  };
+
+  const pressA = () =>
+    fireEvent.keyDown(document, {
+      key: 'a',
+      charCode: 65,
+      code: 'KeyA',
+    });
+
   it('after being rendered for a second an orange square will appear at the top of the grid', fakeAsync(async () => {
     component.ngOnInit();
     let playerPieces = screen.queryAllByTestId('player-peice');
     expect(playerPieces.length).toBe(0);
     tick(1001);
-    playerPieces = await screen.findAllByTestId('player-peice');
+    playerPieces = await getPlayerPieces();
     expect(playerPieces.length).toBe(4);
     expect(playerPieces[0].style.backgroundColor).toBe('orange');
     component.ngOnDestroy();
@@ -46,6 +68,28 @@ describe('BoardComponent', () => {
     const solidPieces = await screen.findAllByTestId('solid-peice');
     expect(solidPieces.length).toBe(4);
     expect(solidPieces[0].style.backgroundColor).toBe('orange');
+    component.ngOnDestroy();
+  }));
+
+  it('the peice will move left if I hit "a" and right if I hit "d"', fakeAsync(async () => {
+    component.ngOnInit();
+    // tick(1000);
+    // const prevCoordinates = await getPlayerCoordinates();
+    // fireEvent.keyDown(document, {
+    //   key: 'a',
+    //   charCode: 65,
+    //   code: 'KeyA',
+    // });
+    // pressA();
+    // const currentCoordinates = await getPlayerCoordinates();
+
+    // const updatedCoordinates = prevCoordinates.map((c) => {
+    //   c.x -= 1;
+    //   return c;
+    // });
+
+    // expect(updatedCoordinates).toEqual(currentCoordinates);
+
     component.ngOnDestroy();
   }));
 });
