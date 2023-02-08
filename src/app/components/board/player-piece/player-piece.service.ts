e11ee907cf235202c51f080469fa3fd4ab100b91
jacob <jshomali@nexient.com>
2023-02-07 20:13:59 -0600

    the piece service has been dried

--- src/app/components/board/player-piece/player-piece.service.ts
+++ src/app/components/board/player-piece/player-piece.service.ts
@@ -3,6 +3,8 @@ import { BehaviorSubject } from 'rxjs';
 import { BoardService } from '../board-service/board.service';
 import { Coordinate } from '../board-service/models';
 
+type Direction = 'left' | 'right' | 'down';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -25,50 +27,62 @@ export class PlayerPieceService {
     );
   }
 
-  moveDown() {
-    const newValue =
-      this.value.length === 0
-        ? [
-            { x: 4, y: 0 },
-            { x: 5, y: 0 },
-            { x: 4, y: 1 },
-            { x: 5, y: 1 },
-          ]
-        : this.value.map((c) => ({ ...c, y: c.y + 1 }));
-    const newlyOccupied = this.getNewlyOccupiedAreas(newValue);
+  private createRandomPiece() {
+    return [
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+      { x: 4, y: 1 },
+      { x: 5, y: 1 },
+    ];
+  }
+
+  private getFuturePosition(direction: Direction) {
+    switch (direction) {
+      case 'down':
+        return this.value.map((c) => ({ ...c, y: c.y + 1 }));
+      case 'left':
+        return this.value.map((c) => ({ ...c, x: c.x - 1 }));
+      case 'right':
+        return this.value.map((c) => ({ ...c, x: c.x + 1 }));
+    }
+  }
+
+  private isInvalidPosition(piece: Coordinate[]) {
+    const newlyOccupied = this.getNewlyOccupiedAreas(piece);
     const currentBoard = this.board.value;
-    const hitTheGround = newlyOccupied.some(
-      (c) =>
-        c.y === currentBoard.length || currentBoard[c.y][c.x].color !== 'white'
-    );
+    return newlyOccupied.some((c) => {
+      const hitGround = c.y === currentBoard.length;
+      if (hitGround) return true;
+      const overlapsWithOtherPiece = currentBoard[c.y][c.x].color !== 'white';
+      if (overlapsWithOtherPiece) return true;
+      const outsideOfBounds = c.x < 0 || c.x > currentBoard[0].length - 1;
+      return outsideOfBounds;
+    });
+  }
+
+  moveDown() {
+    const createNewBlock = this.value.length === 0;
+    const newValue = createNewBlock
+      ? this.createRandomPiece()
+      : this.getFuturePosition('down');
+
+    const hitTheGround = this.isInvalidPosition(newValue);
+
     this.playerPiece.next(hitTheGround ? [] : newValue);
   }
 
   moveLeft() {
-    const newValue = this.value.map((c) => ({
-      ...c,
-      x: c.x - 1,
-    }));
+    const newValue = this.getFuturePosition('left');
 
-    const newlyOccupied = this.getNewlyOccupiedAreas(newValue);
-    const currentBoard = this.board.value;
-    const isInvalid = newlyOccupied.some(
-      (c) => c.x < 0 || currentBoard[c.y][c.x].color !== 'white'
-    );
+    const isInvalid = this.isInvalidPosition(newValue);
 
     !isInvalid && this.playerPiece.next(newValue);
   }
-  moveRight() {
-    const newValue = this.value.map((c) => ({
-      ...c,
-      x: c.x + 1,
-    }));
 
-    const newlyOccupied = this.getNewlyOccupiedAreas(newValue);
-    const currentBoard = this.board.value;
-    const isInvalid = newlyOccupied.some(
-      (c) => c.x > 9 || currentBoard[c.y][c.x].color !== 'white'
-    );
+  moveRight() {
+    const newValue = this.getFuturePosition('right');
+
+    const isInvalid = this.isInvalidPosition(newValue);
 
     !isInvalid && this.playerPiece.next(newValue);
   }
