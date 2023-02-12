ad0729c41ee76c369acf436a4eaab67e544b817e
jacob <jshomali@nexient.com>
2023-02-12 16:51:56 -0600

    created other blocks

--- src/app/components/board/block-generation/block-generation.service.ts
+++ src/app/components/board/block-generation/block-generation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BoardService } from '../board-service/board.service';
 import { Block } from '../board-service/models';
+import { BlockBuilder } from './model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,29 +11,57 @@ export class BlockGenerationService {
 
   private readonly creationIndex = Math.floor(this.board.boardWidth / 2);
 
-  private readonly sqaureBlock = [
-    { x: this.creationIndex, y: 0 },
-    { x: this.creationIndex + 1, y: 0 },
-    { x: this.creationIndex, y: 1 },
-    { x: this.creationIndex + 1, y: 1 },
-  ];
+  private blockBuilder = new BlockBuilder({ x: this.creationIndex, y: 0 });
 
-  private readonly lineBlock = [
-    { x: this.creationIndex, y: 0 },
-    { x: this.creationIndex, y: 1 },
-    { x: this.creationIndex, y: 2 },
-    { x: this.creationIndex, y: 3 },
-  ];
+  private readonly sqaureBlock = this.blockBuilder
+    .addBlockRight()
+    .addBlockBelow()
+    .addBlockLeft()
+    .done();
+
+  private readonly lineBlock = this.blockBuilder
+    .addBlockBelow()
+    .addBlockBelow()
+    .addBlockBelow()
+    .done();
+
+  private readonly SBlock = this.blockBuilder
+    .addBlockBelow()
+    .addBlockRight()
+    .addBlockBelow()
+    .done();
+
+  private readonly ZBlock = this.blockBuilder
+    .addBlockBelow()
+    .addBlockLeft()
+    .addBlockBelow()
+    .done();
+
+  private readonly JBlock = this.blockBuilder
+    .addBlockBelow()
+    .addBlockRight()
+    .addBlockRight()
+    .done();
+
+  private readonly LBlock = this.blockBuilder
+    .addBlockBelow()
+    .addBlockLeft()
+    .addBlockLeft()
+    .done();
+
+  private readonly TBlock = this.blockBuilder
+    .addBlockBelow()
+    .addBlockLeftAndRight()
+    .done();
 
   private allBlocks: Block[] = [
     this.sqaureBlock,
     this.lineBlock,
-    // have not been created yet
-    [],
-    [],
-    [],
-    [],
-    [],
+    this.JBlock,
+    this.LBlock,
+    this.SBlock,
+    this.ZBlock,
+    this.TBlock,
   ];
 
   private blockBag = [...this.allBlocks];
