e6fe19608f504b7f4df039c83eeecb679a4a2e7a
jacob <jshomali@nexient.com>
2023-02-16 14:32:56 -0600

    added an extra test

--- src/app/services/block-movement/validate-movement/validate-movement.service.spec.ts
+++ src/app/services/block-movement/validate-movement/validate-movement.service.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed } from '@angular/core/testing';
+import { BlockBuilder } from 'src/app/components/board/block-generation/model';
+import { BoardService } from 'src/app/components/board/board-service/board.service';
 
 import { ValidateMovementService } from './validate-movement.service';
 
 describe('ValidateMovementService', () => {
   let service: ValidateMovementService;
-
+  let board: BoardService;
+  BoardService;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [BoardService],
+    });
     service = TestBed.inject(ValidateMovementService);
+    board = TestBed.inject(BoardService);
   });
 
   it('should be created', () => {
@@ -21,5 +27,25 @@ describe('ValidateMovementService', () => {
     expect(valid).toBeFalse();
     valid = service.isValidMove([], [{ x: 0, y: -1 }]);
     expect(valid).toBeFalse();
+  });
+
+  it('will return a move is valid if there are no previous blocks there', () => {
+    const valid = service.isValidMove([], [{ x: 0, y: 0 }]);
+    expect(valid).toBeTrue();
+  });
+
+  it('it will return false if another block is in the area', () => {
+    const block = new BlockBuilder({ x: 0, y: 0 }).done();
+    board.lockPieceInplace(block);
+    const valid = service.isValidMove([], block);
+    expect(valid).toBeFalse();
+  });
+
+  it('will not return false if the player overlaps with thier previous position', () => {
+    const block = new BlockBuilder({ x: 0, y: 0 }).addBlockBelow().done();
+    const block2 = new BlockBuilder({ x: 0, y: 1 }).addBlockBelow().done();
+    board.movePiece([], block);
+    const valid = service.isValidMove(block, block2);
+    expect(valid).toBeTrue();
   });
 });
